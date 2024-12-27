ce1648c0dbe92b3614717c494a075cd9333cfe3c
lipebezerra95 <filipebza2019@gmail.com>
2024-12-27 18:18:03 -0300

    feat components

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,6 @@
-import { BannerImage } from "@/components"
+import { BannerImage, FormComponent, StyledH1, StyledP, Logo } from "@/components"
+import { pxToRem } from "@/utils"
+
 import { Box, Container, Grid} from "@mui/material"
 
 
@@ -11,7 +13,24 @@ function Login() {
           <Grid container>
               <Grid item xs={12} sm={6} sx={{ alignItems: 'center', display: 'flex', height: '100vh' }} >
                   <Container maxWidth="sm">
-                    <h1>Login</h1>  
+                    <Box sx={{ marginBottom: pxToRem(24) }}><Logo height={41} width={100}/></Box>
+                    <Box sx={{ marginBottom: pxToRem(24) }}>
+                      <StyledH1>Bem vindo</StyledH1> 
+                      <StyledP>Digite sua senha e email para logar</StyledP>
+                    </Box>                 
+                    <FormComponent 
+                       inputs={[
+                        { type: 'email', placeholder: 'Email', disabled: true},
+                        { type: 'password', placeholder: 'senha' },
+                       ]}
+                       buttons={[
+                        { className: 'primary', type: 'submit', children: 'Login', disabled: true },
+                       ]}
+                       message={{
+                        msg: 'secesso!!!',
+                        type: 'sucess',
+                       }}
+                    /> 
                   </Container>
               </Grid>
               <Grid 
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,6 +1,7 @@
 
 import { Box, Container, Grid} from "@mui/material"
-import { BannerImage } from "@/components"
+import { BannerImage, FormComponent, Logo, StyledH1, StyledP, StyledUl  } from "@/components"
+import { pxToRem } from "@/utils"
 
 function Registration() {
   
@@ -11,7 +12,30 @@ function Registration() {
                  <Grid container>
                      <Grid item xs={12} sm={6} sx={{ alignItems: 'center', display: 'flex', height: '100vh' }} >
                          <Container maxWidth="sm">
-                         <h1>CADASTRO</h1>    
+                         <Box sx={{ marginBottom: pxToRem(24) }}><Logo height={41} width={100}/></Box>
+                          <Box sx={{ marginBottom: pxToRem(24) }}>
+                            <StyledH1>Faça seu cadastro</StyledH1> 
+                            <StyledP>Primeiro diga nos quem voce é.</StyledP>
+                            <StyledUl>
+                                  <li>Entre 8 e 16 caracteres;</li>
+                                  <li>Pelo menos uma letra Maiuscula;</li>
+                                  <li>Pelo menos um caracteere especial;</li>
+                                  <li>pelo menos um numero;</li>
+                            </StyledUl>
+                          </Box>     
+                         <FormComponent 
+                       inputs={[
+                        { type: 'email', placeholder: 'Email'},
+                        { type: 'password', placeholder: 'senha' },
+                       ]}
+                       buttons={[
+                        { className: 'primary', type: 'submit', children: 'Login'},
+                       ]}
+                       message={{
+                        msg: 'erro',
+                        type: 'error',
+                       }}
+                    />    
                          </Container>
                      </Grid>
                      <Grid 
