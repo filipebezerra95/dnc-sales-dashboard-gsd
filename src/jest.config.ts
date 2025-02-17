bbbeb047462494333a0e9a8285c26d565f045ac4
lipebezerra95 <filipebza2019@gmail.com>
2025-02-17 11:56:22 -0300

    feat/adding-unit-tests-1

--- src/jest.config.ts
+++ src/jest.config.ts
@@ -1,11 +1,11 @@
 export default {
     testEnvironment: 'jsdom',
     transform: {
-    '^.+\.tsx?$': 'ts-jest'
+    '^.+.tsx?$': 'ts-jest'
     },
     moduleNameMapper: {
-    '\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    '\.svg$': 'jest-transform-svg',
+    '.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '.svg$': 'jest-transform-svg',
     '@/(.*)$': '<rootDir>/src/$1'
     },
     //setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
