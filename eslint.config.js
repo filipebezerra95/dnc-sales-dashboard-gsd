f1a3101ecbaab213f86838ff7e472b65466f50a3
lipebezerra95 <filipebza2019@gmail.com>
2025-02-17 11:45:44 -0300

    feat/adding-e2e-tests

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,17 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintPluginCypress from 'eslint-plugin-cypress'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, ...eslintPluginPrettier.configs.prettier],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...eslintPluginPrettier.configs.prettier,
+      ...eslintplugincypress.configs.cypress,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -25,5 +31,5 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  },
+  }
 )
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import { BrowserRouter as  Router, Route, Routes, Navigate, Outlet } from "react-router-dom"
-import Cookies from "js-cookie"
-import { Home, Leads, Login, Profile, Registration } from "./pages"
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  Outlet,
+} from 'react-router-dom'
+import Cookies from 'js-cookie'
+import { Home, Leads, Login, Profile, Registration } from './pages'
 
 function App() {
   const ProtectedRoute = () => {
@@ -11,17 +17,16 @@ function App() {
     }
     return <Outlet />
   }
-  
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/cadastro" element={<Registration/>}/>
-        <Route element={<ProtectedRoute/>}>
-            <Route path="/home" element={<Home/>}/>
-            <Route path="/leads" element={<Leads/>}/>
-            <Route path="/perfil" element={<Profile/>}/>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Registration />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/leads" element={<Leads />} />
+          <Route path="/perfil" element={<Profile />} />
         </Route>
       </Routes>
     </Router>
